3f5edaf884f41209894073333c4cf552ea247450
Arthur <arthur.barthelme78@gmail.com>
2026-05-10 01:48:51 +0200

    ♻️ Refactor storage connection handling
    Update storage connection logic to use StorageConfig for better clarity
    and maintainability. This change improves the structure of the code
    and prepares for future enhancements.

--- src/main/java/fr/milekat/banks/Main.java
+++ src/main/java/fr/milekat/banks/Main.java
@@ -12,7 +12,9 @@ import fr.milekat.utils.storage.StorageConnection;
 import fr.milekat.utils.storage.StorageLoader;
 import fr.milekat.utils.storage.StorageVendor;
 import fr.milekat.utils.storage.adapter.elasticsearch.connection.ESConnection;
+import fr.milekat.utils.storage.adapter.sql.connection.SQLConnection;
 import fr.milekat.utils.storage.exceptions.StorageLoadException;
+import fr.milekat.utils.storage.utils.StorageConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -163,8 +165,9 @@ public class Main extends JavaPlugin {
         try {
             getStorage().disconnect();
         } catch (Exception ignored) {}
-        StorageConnection connection = new StorageLoader(config, logger).getLoadedConnection();
-        if (Objects.requireNonNull(connection.getVendor()) == StorageVendor.ELASTICSEARCH) {
+        StorageConfig storageConfig = StorageConfig.fromConfig(config);
+        StorageConnection connection = new StorageLoader(storageConfig, logger).getLoadedConnection();
+        if (storageConfig.type() == StorageVendor.ELASTICSEARCH) {
             STORAGE = new ESStorage((ESConnection) connection, config);
         } else {
             throw new StorageLoadException("Unsupported storage type");
